238b3c74eef7566c2667f3d5b8c8401dc452f757
Dmitry <kopdevile@gmail.com>
2025-09-18 12:27:40 +0700

    fix(api): sort imports in serializers; use Decimal('0.01')

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 
 
 class RoomCreateSerializer(serializers.Serializer):
     description = serializers.CharField()
-    price = serializers.DecimalField(max_digits=10, decimal_places=2, min_value=0.01)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2, min_value=Decimal("0.01"))
