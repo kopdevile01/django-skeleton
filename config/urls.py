139833847c851ee1d747e370a3376144f477a22c
Dmitry <kopdevile@gmail.com>
2025-09-18 12:44:22 +0700

    feat(api): add DELETE /rooms/<id>/ route

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from hotel.views import rooms_collection
+from hotel.views import rooms_collection, rooms_item
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("rooms/", rooms_collection),
+    path("rooms/<int:room_id>/", rooms_item),
 ]
