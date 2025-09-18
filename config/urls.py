b82640b0d08a44916f4c1a8aa2ffdeccfdc25480
Dmitry <kopdevile@gmail.com>
2025-09-18 22:08:55 +0700

    feat(api): add booking serializers and routes

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 
-from hotel.views import rooms_collection, rooms_item
+from hotel.views import (
+    bookings_create,
+    bookings_delete,
+    bookings_list,
+    rooms_collection,
+    rooms_item,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("rooms/", rooms_collection),
     path("rooms/<int:room_id>/", rooms_item),
+    path("bookings/create/", bookings_create),
+    path("bookings/list/", bookings_list),
+    path("bookings/delete/", bookings_delete),
 ]
